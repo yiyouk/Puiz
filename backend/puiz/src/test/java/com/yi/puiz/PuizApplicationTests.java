57d0eb7df25a822a51d452141f282d837f70610f
yiyouk <yiyou00@naver.com>
2023-09-13 21:24:27 +0900

    [Inin] init backend code

---
+++ backend/puiz/src/test/java/com/yi/puiz/PuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yi.puiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
